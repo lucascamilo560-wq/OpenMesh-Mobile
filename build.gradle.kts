16365b1e3239a0b68a32eb2e0901b02935e00489
lucascamilo560-wq <lucascamilo560@gmail.com>
2026-08-19 16:02:46 -0300

    build: configure Android and Kotlin plugins

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "9.3.0" apply false
+    id("com.android.library") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    kotlin("jvm") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
+}
