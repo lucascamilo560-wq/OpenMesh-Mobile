a5f43b0c7433067d8cc627ce603843aa979d4c4f
lucascamilo560-wq <lucascamilo560@gmail.com>
2026-08-19 16:12:25 -0300

    build(demo): add Android test application module

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.openmesh.demo"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.openmesh.demo"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":mesh-android"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+}
