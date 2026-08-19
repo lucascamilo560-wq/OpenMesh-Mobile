ebc88b85c7d9ad8ef0e0307a98859bf4e3ecf071
lucascamilo560-wq <lucascamilo560@gmail.com>
2026-08-19 16:03:13 -0300

    build(core): add mesh-core module

---
+++ mesh-core/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
