708e201749f38bc0f7c5e6a370659276ecb2c07a
lucascamilo560-wq <lucascamilo560@gmail.com>
2026-08-19 16:32:30 -0300

    feat(android): reserve GATT identity characteristics

--- mesh-android/src/main/kotlin/com/openmesh/android/BleMeshProtocol.kt
+++ mesh-android/src/main/kotlin/com/openmesh/android/BleMeshProtocol.kt
@@ -7,6 +7,8 @@ object BleMeshProtocol {
     val SERVICE_UUID: UUID = UUID.fromString("8d53d7a0-6f4b-4f78-a6b6-2ecb49ce6b01")
     val RX_CHARACTERISTIC_UUID: UUID = UUID.fromString("8d53d7a1-6f4b-4f78-a6b6-2ecb49ce6b01")
     val TX_CHARACTERISTIC_UUID: UUID = UUID.fromString("8d53d7a2-6f4b-4f78-a6b6-2ecb49ce6b01")
+    val NODE_ID_CHARACTERISTIC_UUID: UUID = UUID.fromString("8d53d7a3-6f4b-4f78-a6b6-2ecb49ce6b01")
+    val PUBLIC_KEY_CHARACTERISTIC_UUID: UUID = UUID.fromString("8d53d7a4-6f4b-4f78-a6b6-2ecb49ce6b01")
     val CCCD_UUID: UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
 
     val SERVICE_PARCEL_UUID = ParcelUuid(SERVICE_UUID)
